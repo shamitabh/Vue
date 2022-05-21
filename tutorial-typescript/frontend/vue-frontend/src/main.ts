301e53c924d53301a02617d80bec04bead321263
shamitabh <pal185789@gmail.com>
2022-05-22 02:09:15 +0530

    Home page created.

--- tutorial-typescript/frontend/vue-frontend/src/main.ts
+++ tutorial-typescript/frontend/vue-frontend/src/main.ts
@@ -1,6 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import axios from "axios";
 
-createApp(App).use(store).use(router).mount('#app')
+// set base url for all api requests
+axios.defaults.baseURL = process.env.VUE_APP_BASE_API;
+
+createApp(App).use(store).use(router, axios).mount("#app");
