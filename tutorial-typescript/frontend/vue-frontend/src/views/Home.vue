3036d96392ac2f7dbc0ba5fa3c3bf84e03c72f0e
shamitabh <pal185789@gmail.com>
2022-05-21 21:12:25 +0530

    Header and Footer added.

---
+++ tutorial-typescript/frontend/vue-frontend/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">Home</div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({})
+export default class HomeView extends Vue {
+  name: string;
+}
+</script>
